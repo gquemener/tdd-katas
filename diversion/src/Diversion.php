ca4df7637474462bc7c88abc9525773ff65fcf9d
Gildas Quéméner <gildas.quemener@gmail.com>
2021-11-24 21:45:51 +0100

    Add comment

--- diversion/src/Diversion.php
+++ diversion/src/Diversion.php
@@ -8,6 +8,10 @@ final class Diversion
 {
     public function countFor(int $n): int
     {
+        /**
+         * This sequence does not follow Fibonacci sequence as defined on https://oeis.org/A000045
+         * at the beginning but converge after a few iteration
+         */
         return match ($n) {
             0 => 0,
             1 => 2,
