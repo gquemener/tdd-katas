23969810067056795ff487a0128809acc44f5d36
Gildas Quéméner <gildas.quemener@gmail.com>
2021-11-24 21:36:04 +0100

    [Diversion] Fix incorrect inital case

--- diversion/src/Diversion.php
+++ diversion/src/Diversion.php
@@ -8,6 +8,9 @@ final class Diversion
 {
     public function countFor(int $n): int
     {
+        if (0 === $n) {
+            return 0;
+        }
         $result = 0;
         foreach (range(0, (2**$n)-1) as $value) {
             if (!str_contains(decbin($value), '11')) {
