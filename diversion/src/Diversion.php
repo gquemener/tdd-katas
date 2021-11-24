e88037deac30c5379d1ea34f42b33dc0310edb30
Gildas Quéméner <gildas.quemener@gmail.com>
2021-11-24 21:39:06 +0100

    [Diversion] Simplify diversion implementation

--- diversion/src/Diversion.php
+++ diversion/src/Diversion.php
@@ -8,18 +8,11 @@ final class Diversion
 {
     public function countFor(int $n): int
     {
-        if (0 === $n) {
-            return 0;
-        }
-
-        if (1 === $n) {
-            return 2;
-        }
-
-        if (2 === $n) {
-            return 3;
-        }
-
-        return $this->countFor($n - 1) + $this->countFor($n - 2);
+        return match ($n) {
+            0 => 0,
+            1 => 2,
+            2 => 3,
+            default => $this->countFor($n - 1) + $this->countFor($n - 2)
+        };
     }
 }
